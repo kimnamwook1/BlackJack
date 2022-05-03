bfcf0bce10b8d4d34c6aa94adeecd74925a0312e
KimNamWook <7922378@naver.com>
2022-05-03 17:16:44 +0900

    주석 추가

--- Common.h
+++ Common.h
@@ -1,10 +1,10 @@
 #pragma once
 
-//#ifndef _COMMON_H_
-//#define _COMMON_H_//pragma once가 지원 안할때 쓰는 문법==컴파일러 버전이 달라서 안되기 때문에 pragma once 와 기왕이면 같이 사용
+#ifndef __COMMON_H__
+#define __COMMON_H__//pragma once가 지원 안할때 쓰는 문법==컴파일러 버전이 달라서 안되기 때문에 pragma once 와 기왕이면 같이 사용
 
 #define MAX 52
 extern int A[MAX];
 extern int SplitCard[52]; // 전역 변수는 초기화 해주지마
 
-//#endif //ifdef 또는 ifndef는 endif와 짝꿍
+#endif //ifdef 또는 ifndef는 endif와 짝꿍
