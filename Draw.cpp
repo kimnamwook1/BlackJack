d8900427ce5ba720c496d1dea82a5378e05944fb
KimNamWook <7922378@naver.com>
2022-05-09 11:04:24 +0900

    3장 뽑아서 숫자 비교

--- Draw.cpp
+++ Draw.cpp
@@ -1,30 +1,18 @@
 #include "Draw.h"
+#include "Common.h"
 
 using namespace std;
+
+int PlayerSum = 0;
+int DealerSum = 0;
+int Deck[52] = { 0, };
 
 void Draw()
 {
 	cout << "Player" << '\t' << "  Dealer" << endl;
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 6; ++i)
 	{
-		int Type = SplitCard[i] / 13;
 		int Number = SplitCard[i] % 13;
-
-		switch (Type)
-		{
-		case 0:
-			cout << "¢¼";
-			break;
-		case 1:
-			cout << "¢À";
-			break;
-		case 2:
-			cout << "¢¾";
-			break;
-		case 3:
-			cout << "¡ß";
-			break;
-		}
 
 		switch (Number)
 		{
@@ -43,22 +31,53 @@ void Draw()
 		default:
 			cout << Number + 1 << " ";
 			break;
+
 		}
 
 		cout << ((i % 2 == 0) ? '\t' : '\n');
+
+	}
+	for (int j = 0; j < 6; ++j)
+	{
+		Deck[j] = SplitCard[j] % 13 + 1;
 	}
 
-	if (Player[0] + Player[1] < Dealer[0] + Dealer[1])
+	int Player[3] = { 0, };
+	int Dealer[3] = { 0, };
+	for (int i = 0; i < 3; i++)
 	{
-		cout << "Dealer win" << endl;
+		Player[i] = Deck[i * 2];
+		Dealer[i] = Deck[i * 2 + 1];
 	}
-	else if (Player[0] + Player[1] > Dealer[0] + Dealer[1])
+
+	for (int k = 0; k < 3; k++)
 	{
-		cout << "Player win" << endl;
+		PlayerSum += (Player[k] > 10) ? 10 : Player[k];
+		DealerSum += (Dealer[k] > 10) ? 10 : Dealer[k];
+
 	}
+	cout << PlayerSum << ":" << DealerSum << endl;
+
+	if (DealerSum > 21)
+
+		cout << "Player Win" << endl;
+
+	else if (PlayerSum < 21 && DealerSum < 21)
+
+		if (PlayerSum > DealerSum)
+
+			cout << "Player Win" << endl;
+
+		else if (PlayerSum == DealerSum)
+
+			cout << "Pealer Win" << endl;
+
+		else
+			cout << "Dealer Win" << endl;
 	else
-	{
-		cout << "Draw" << endl;
-	}
+
+		cout << "Player Win" << endl;
+
+
 
 }
--- Split.cpp
+++ Split.cpp
@@ -6,13 +6,5 @@ void Split()
 	{
 		SplitCard[i] = A[i];
 
-		for (int j = 0; j < 11; ++j)
-		{
-			for (int k = 0; k < 11; k = k + 2)
-			{
-				Player[j] = SplitCard[k];
-				Dealer[j] = SplitCard[k + 1];
-			}
-		}
 	}
 }
